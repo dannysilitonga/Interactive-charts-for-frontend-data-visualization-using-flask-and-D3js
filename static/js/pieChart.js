82770fca61544702859c718dced2883931ef7263
dannysilitonga <dsilitonga@gradcenter.cuny.edu>
2023-04-29 21:00:58 +0000

    practice run

--- static/js/pieChart.js
+++ static/js/pieChart.js
@@ -1,72 +1,71 @@
 function d3PieChart(dataset, datasetBarChart){
-    // Set up SVG dimensions and properties 
-    const margin = {top:20, right:20, bottom:20, left:20};
-    const width = 350 - margin.left - margin.right,
+    // Set up SVG dimensions and properies
+    const margin = {top:20, right: 20, bottom: 20, left:20};
+    const width = 350 - margin.left - margin.right, 
     height = 350 - margin.top - margin.bottom,
-    outerRadius = Math.min(width, height) / 2, 
+    outerRadius = Math.min(width, height) /2,
     innerRadius = outerRadius * .5,
     color = d3.scaleOrdinal(d3.schemeAccent); //color scheme
 
-    // Selecting the div with id pieChart on the index.html template file
+    // Selecting the div and id pieChart on the index.html template
     const visualization = d3.select('#pieChart')
-        .append("svg")      //Injecting an SVG element
-        .data([dataset])    //Binding the pie chart data
+        .append("svg")  //Injecting an SVG element
+        .data([dataset]) //Binding the pie chart data
         .attr("width", width)
-        .attr("height", height) 
-        .append("g")        //Grouping the various SVG components   
-        .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")"); //Piechart tranformation and transition upon page loading
+        .attr("height", height)
+        .append("g") //Grouping the various SVG components
+        .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")"); //Piechart transformation and transition upon page loading )
 
-    const data = d3.pie()   //Creating the data object that will develop the various segment of the pie chart.
+    const data = d3.pie() //Creating the data object that will develop the various segment of the pie chart.    
         .sort(null)
-        .value(function(d){return d.value;})(dataset);    // Retrieve the pie chart data values from our Flask app, the pie chart where tied to a 'value' key of a JSON object.
+        .value(function(d){return d.value;})(dataset);   //Retrieve the pie chart data values from our Fask app, the pie chart where tied to a 'value' key of a JSON object. 
 
-    
     // Generate an arc generator that produces the circular chart (outer circle)
-    const arc = d3.arc()    
+    const arc = d3.arc()
         .outerRadius(outerRadius)
         .innerRadius(0);
-
-     // Generate an arc generator that produces the circular chart (inner circle)
+    
+    //Generate an arc generator that produces the circular chart (inner circle)
     const innerArc = d3.arc()
         .innerRadius(innerRadius)
         .outerRadius(outerRadius);
 
     // Create pie chart slices based on the data object created
     const arcs = visualization.selectAll("g.slice")
-        .data(data)                     
-        .enter()    // creates the initial join of data to elements                       
-        .append("svg:g")               
-        .attr("class", "slice") 
-    	.on("click", click);
-    				
-    arcs.append("svg:path")     // create path element 
-        .attr("fill", function(d, i) { return color(i); } )     //Add color to slice
-        .attr("d", arc)     // creates actual SVG path with associated data and the arc drawing function
-		.append("svg:title")        // Add title to each piechart slice 
-        .text(function(d) { return d.data.category + ": " + d.data.value+"%"; });			
+        .data(data)
+        .enter() // creates the initial join of data to elements
+        .append("svg:g")
+        .attr("class", "slice")
+        .on("click", click); 
 
-    d3.selectAll("g.slice")     // select slices in the group SVG element (pirchart)
+    arcs.append("svg:path") // create path element
+        .attr("fill", function(d, i) { return color(i); } ) //Add color to slice
+        .attr("d", arc)
+        .append("svg: title") // Add title to each piechart slice
+        .text(function(d) { return d.data.category + ": " + d.data.value+"%"; });
+      
+    d3.selectAll("g.slice") //select slices in the group SVG element (piechart)
         .selectAll("path")
-        .transition()           //Set piechart transition on loading
-		.duration(200)
-		.delay(5)
-        .attr("d", innerArc);
+        .transition()
+        .duration(200)
+        .delay(5)
+        .attr("d", innerArc); 
 
-    arcs.filter(function(d) { return d.endAngle - d.startAngle > .1; })     //Define slice labels at certain angles
-        .append("svg:text")     //Insert text area in SVG
-        .attr("dy", "0.20em")      //shift along the y-axis on the position of text content
-        .attr("text-anchor", "middle")      //Position slice labels
-        .attr("transform", function(d) { return "translate(" + innerArc.centroid(d) + ")"; }) //Positioning upon transition and transform
-        .text(function(d) { return d.data.category; }); // Append category name on slices
+    arcs.filter(function(d) { return d.endAngle - d.startAngle > .1; }) //Define slice labels at certain angles.
+        .append("svg:text")  //Insert text area in SVG
+        .attr("dy", "0.20em")  //shift along the y-axis on the position of text content 
+        .attr("text-anchor", "middle")  //Position slice labels
+        .attr("transform", function(d) { return "translate(" + innerArc.centroid(d) + ")"; }) //Positioning upon and transform
+        .text(function(d) { return d.data.category; }); //Append category name on slices
 
-    visualization.append("svg:text") //Append the title of chart in the middle of the pie chart
+    visualization.append("svg:text") //Append the tile of chart in the middle of the pie chart
         .attr("dy", ".20em")
         .attr("text-anchor", "middle")
         .text("churned customers")
-        .attr("class","title");		    
+        .attr("class", "title");
 
-    // Function to update barchart when a piechart slice is clicked
+    //Function to update barchart when a piechart slice is clicked
     function click(d, i) {
         updateBarChart(d.data.category, color(i), datasetBarChart);
-     }
-}
+    }
+}   
--- static_nonds/js/barChart.js
+++ static_nonds/js/barChart.js
@@ -1,17 +1,18 @@
-//Set up SVG dimensions and properties
-const margin = {top: 20, right: 10, bottom:20, left:20},
-width = 350 - margin.left - margin.right, 
-height = 350 - margin.top - margin.bottom,
-barPadding = 5,
-graph_misc = {ylabel: 4, xlabelH:5, title:9};
 
-//Setting the default group
+//Set up SVG dimensions and properties
+const margin = {top: 20, right: 10, bottom: 20, left: 20},
+width = 350 - margin.left - margin.right,
+height = 350 - margin.top - margin.bottom,
+barPadding = 7,
+graph_misc = {ylabel:4, xlabelH :4, title:9};
+
+// Setting the default group
 const group = "All";
 
-// Function to get the percentage values for a specific selected group
+// Function to get the percentage values  for a specific selected group from the whole dataset.
 function get_percentage(group, datasetBarChart){
     const _ = [];
-    for (instance in datasetBarChart) {
+    for (instance in datasetBarChart){
         if (datasetBarChart[instance].group==group){
             _.push(datasetBarChart[instance])
         }
@@ -21,14 +22,14 @@ function get_percentage(group, datasetBarChart){
 function d3BarChart(datasetBarChart){
     defaultBarChart = get_percentage(group, datasetBarChart); 
 
-    const xScale = d3.scaleLinear() //Barchart X axis scale
-        .domain([0, defaultBarChart.length]) //Scale range from 0 to the length of data object
+    const xScale = d3.scaleLinear()     // Barchart X axis scale
+        .domain([0, defaultBarChart.length]) // Scale range from 0 to the length of data object
         .range([0, width]);
 
     const yScale = d3.scaleLinear() // Barchart y axis scale
-        .domain([0, d3.max(defaultBarChart, function(d) { return d.value; })])
+        .domain([0, d3.max(defaultBarChart, function(d) { return d.value; })])    //Scale range from 0 to the maximum value of the default bar chart data
         .range([height, 0]);
-    
+
     // // Selecting the div with id barChart on the index.html template file
     const bar = d3.select('#barChart')
         .append('svg')
@@ -40,13 +41,14 @@ function d3BarChart(datasetBarChart){
     bar.append('text')
         .attr('x', (width + margin.left + margin.right)/2)
         .attr('y', graph_misc.title)
-        .attr('class', 'title')
+        .attr('class','title')				
         .attr('text-anchor', 'middle')
-        .text('Tenure group for churned customer');
+        .text('Tenure group for churned customers');
 
     const visualization = bar.append('g')
         .attr("transform", "translate(" + margin.left + "," + (margin.top + graph_misc.ylabel) + ")");
 
+    //Selecting all the bar serving as bins of data
     visualization.selectAll("rect")
         .data(defaultBarChart)
         .enter()
@@ -54,38 +56,37 @@ function d3BarChart(datasetBarChart){
         .attr("x", function(d, i) {
             return xScale(i);
         })
-        .attr("width", width / defaultBarChart.length - barPadding)
+        .attr("width", width / defaultBarChart.length - barPadding)   
         .attr("y", function(d) {
             return yScale(d.value);
-        })
+        })  
         .attr("height", function(d) {
             return height-yScale(d.value);
         })
         .attr("fill", "#757077");
-
-    //Adding barchart labels
+    
+    //Adding  barchart labels
     visualization.selectAll('text')
         .data(defaultBarChart)
         .enter()
         .append("text")
         .text(function(d) {
-            return d.value+"%";
+                return d.value+"%";
         })
         .attr("text-anchor", "middle")
 
         .attr("x", function(d, i) {
-            return (i * (width/ defaultBarChart.length)) + ((width / defaultBarChart.length - barPadding) /2);
-
+                return (i * (width / defaultBarChart.length)) + ((width / defaultBarChart.length - barPadding) / 2);
         })
         .attr("y", function(d) {
-            return (yScale(d.value) - graph_misc.ylabel); //Setting the Y axis to represent the value in the served JSON data
-
+                return (yScale(d.value) - graph_misc.ylabel); //Setting the Y axis to represent the value in the served JSON data
         })
         .attr("class", "yAxis");
+    
 
     const xLabels = bar
         .append("g")
-        .attr("transform", "translate(" + maegin.left + "," + (margin.top + height + graph_misc.xlabelH) + ")"); 
+        .attr("transform", "translate(" + margin.left + "," + (margin.top + height + graph_misc.xlabelH)  + ")");
 
     xLabels.selectAll("text.xAxis")
         .data(defaultBarChart)
@@ -94,11 +95,9 @@ function d3BarChart(datasetBarChart){
         .text(function(d) { return d.category;})
         .attr("text-anchor", "middle")
         .attr("x", function(d, i) {
-            return (i * (width / defaultBarChart.length)) + ((width / defaultBarChart.length - barPadding) /2);   
+            return (i * (width / defaultBarChart.length)) + ((width / defaultBarChart.length - barPadding) / 2);
         })
         .attr("y", 15)
-        .attr("class", "xAxis");
-    }
-
-
+        .attr("class", "xAxis"); 		    
 }
+
